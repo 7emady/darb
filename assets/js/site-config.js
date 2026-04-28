351525437eab9c9b5dd6616115347eebb3616982
7emady <mal7emady5579@gmail.com>
2026-04-28 16:18:43 -0700

    Update Google Sheets form endpoint

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -1,8 +1,8 @@
 window.DARB_SITE_CONFIG = {
-  phone: "0567436557",
+  phone: "966567436557",
   whatsapp: "https://wa.me/966567436557",
   email: "info@darb-altahadi.sa",
   siteUrl: "https://darb-altahadi.sa/",
   googleSheetsWebAppUrl:
-    "https://script.google.com/macros/s/AKfycbz1qAwCVwDmaqAldtuulmDep65qtg8IDTlMtfBpZdyEi0dMqtSfH7aaEExiMDapVgki/exec",
+    "https://script.google.com/macros/s/AKfycbwKc4qf9k4v3Azt1n_tYUNeTv0go-9PJAyYoJzL42LfzFnG24RMD4_7cjzqFHUNRrc0mw/exec",
 };
